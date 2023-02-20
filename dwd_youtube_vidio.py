f52a7cf28c27569cfbcaecf1bfb1b7c6e3a04d17
Suman_p <sumansumanth513@gmail.com>
2023-02-20 14:14:45 +0530

    here you can download youtube vidoes using python

--- dwd_youtube_vidio.py
+++ dwd_youtube_vidio.py
@@ -1,10 +1,12 @@
 #import pytube library to download the vidio
 import pytube
+
 #Ask for the url of vidio
 url1=input("enter url:")
+
 #we can take path as well
 path=input("enter path of  storage:")
+
 #here the line to download the vidio
 pytube.YouTube(url1).streams.get_highest_resolution().download(path)
 print("download successfully")
-
